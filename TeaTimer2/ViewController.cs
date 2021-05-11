025712523ad45e117deebf1c7ba99e4ca9283931
Timothy Reynolds <tim@mahonkin.com>
2021-05-11 13:30:43 -0400

    Added the timer and timer event handler

--- TeaTimer2/ViewController.cs
+++ TeaTimer2/ViewController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Timers;
 using AppKit;
 using Foundation;
 
@@ -6,7 +7,9 @@ namespace TeaTimer
 {
     public partial class ViewController : NSViewController
     {
-        private bool isRunning = false;
+        private static Timer timer = new Timer(1000.0);
+        private TimeSpan steepTime;
+        private NSButton startStopButton;
 
         public ViewController(IntPtr handle) : base(handle)
         {
@@ -17,23 +20,28 @@ namespace TeaTimer
             base.ViewDidLoad();
 
             // Do any additional setup after loading the view.
+            // Setup the timer callback
+            timer.AutoReset = true;
+            timer.Elapsed += elapsedEventHandler;
         }
 
-        partial void ButtonClicked(NSObject sender) 
+        partial void ButtonClicked(NSObject sender)
         {
-            NSButton startStopButton = (NSButton)sender;
+            startStopButton = (NSButton)sender;
 
-            if (isRunning)
+            if (timer.Enabled)
             {
-                isRunning = false;
+                timer.Stop();
+                steepTime = new TimeSpan(0, 0, 30);
                 startStopButton.Title = "Start";
                 TimerLabel.StringValue = "Choose a Variety";
             }
             else
             {
-                isRunning = true;
+                steepTime = new TimeSpan(0, 0, 30);
+                timer.Start();
                 startStopButton.Title = "Stop";
-                TimerLabel.StringValue = "Countdown started";
+                TimerLabel.StringValue = steepTime.ToString();
             }
         }
 
@@ -48,6 +56,28 @@ namespace TeaTimer
                 base.RepresentedObject = value;
                 // Update the view, if already loaded.
             }
+        }
+
+        private void elapsedEventHandler(object source, ElapsedEventArgs args)
+        {
+            TimerLabel.InvokeOnMainThread(() => UpdateLabel()) ;
+        }
+
+        private void UpdateLabel()
+        {
+            if (steepTime.TotalSeconds > 0)
+            {
+
+                steepTime = steepTime.Subtract(new TimeSpan(0, 0, 1));
+                TimerLabel.StringValue = steepTime.ToString();
+            }
+            else
+            {
+                timer.Stop();
+                startStopButton.Title = "Start";
+                TimerLabel.StringValue = "Tea is Ready!";
+            }
+
         }
     }
 }
