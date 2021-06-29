83ac033a6cb4dd49293c18a63a478eadefe05814
Timothy Reynolds <tim@mahonkin.com>
2021-06-29 17:45:11 -0400

    Removed the sqlite database usage

--- TeaTimer/AppDelegate.cs
+++ TeaTimer/AppDelegate.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.IO;
-using AppKit;
+﻿using AppKit;
 using Foundation;
-using SQLite;
 
 namespace TeaTimer
 {
     [Register("AppDelegate")]
     public class AppDelegate : NSApplicationDelegate
     {
-        private string _dbPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData, Environment.SpecialFolderOption.Create) + Path.DirectorySeparatorChar + "TeaTimer" + Path.DirectorySeparatorChar + "TeaTimer.db3";
-
         public AppDelegate()
         {
         }
@@ -18,17 +13,6 @@ namespace TeaTimer
         public override void WillFinishLaunching(NSNotification notification)
         {
             // Insert code here to initialize your application
-
-            // Get the database connection
-            try
-            {
-                InitializeDatabase();
-            }
-            catch (Exception ex)
-            {
-                File.Delete(_dbPath);
-                Console.WriteLine(ex.Message);
-            }
         }
 
         public override void DidFinishLaunching(NSNotification notification)
@@ -40,19 +24,5 @@ namespace TeaTimer
         {
             // Insert code here to tear down your application
         }
-
-        private void InitializeDatabase()
-        {
-            if (File.Exists(_dbPath) == false)
-            {
-                Directory.CreateDirectory(Path.GetDirectoryName(_dbPath));
-                File.Create(_dbPath);
-                SQLiteConnection conn = new SQLiteConnection(_dbPath);
-                TeaModel earlGrey = new TeaModel("Earl Grey", new TimeSpan(0, 1, 20), 185);
-                conn.CreateTable<TeaModel>();
-                conn.Insert(earlGrey);
-            }
-        }
-
     }
 }
