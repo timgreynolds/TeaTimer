684c2a10e65fca97a88d1cbdeac44c0135f5e146
Timothy Reynolds <tim@mahonkin.com>
2021-07-05 18:49:25 -0400

    Moved the datastore loading code

--- TeaTimer/AppDelegate.cs
+++ TeaTimer/AppDelegate.cs
@@ -1,23 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using AppKit;
+﻿using AppKit;
 using Foundation;
-using Newtonsoft.Json;
 
 namespace TeaTimer
 {
     [Register("AppDelegate")]
     public class AppDelegate : NSApplicationDelegate
     {
-        private List<TeaModel> _teas;
-
-        public List<TeaModel> Teas
-        {
-            get => _teas;
-            set => _teas = value;
-        }
-
         public AppDelegate()
         {
         }
@@ -25,40 +13,6 @@ namespace TeaTimer
         public override void WillFinishLaunching(NSNotification notification)
         {
             // Insert code here to initialize your application
-            try
-            {
-                _teas = TeaVarietiesDataSource.InitializeDatabase();
-            }
-            catch (IOException ioException)
-            {
-                NSAlert alert = new NSAlert()
-                {
-                    AlertStyle = NSAlertStyle.Critical,
-                    MessageText = "An error occurred trying to open the tea varieties database.",
-                    InformativeText = $"Message: {ioException.Message}"
-                };
-                alert.RunModal();
-            }
-            catch (JsonSerializationException serializationException)
-            {
-                NSAlert alert = new NSAlert()
-                {
-                    AlertStyle = NSAlertStyle.Critical,
-                    MessageText = "An error occurred trying to load the list of tea varieties.",
-                    InformativeText = $"Message: {serializationException.Message}"
-                };
-                alert.RunModal();
-            }
-            catch (Exception exception)
-            {
-                NSAlert alert = new NSAlert()
-                {
-                    AlertStyle = NSAlertStyle.Critical,
-                    MessageText = "An error occurred.",
-                    InformativeText = $"Message: {exception.Message}"
-                };
-                alert.RunModal();
-            }
         }
 
         public override void DidFinishLaunching(NSNotification notification)
