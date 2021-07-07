cd9d111c7c1374341b2fec5a433ce9247724e4a4
Timothy Reynolds <tim@mahonkin.com>
2021-07-07 12:49:42 -0400

    Added a custom exception class

---
+++ TeaTimer/TeaModelException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace TeaTimer
+{
+    [Serializable]
+    internal class TeaModelException : Exception
+    {
+        public TeaModelException()
+        {
+        }
+
+        public TeaModelException(string message) : base(message)
+        {
+        }
+
+        public TeaModelException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected TeaModelException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
