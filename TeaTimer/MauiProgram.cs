d37e032d2e6a02a1f9a3a9ed5e792f31849e09c6
Timothy Reynolds <tim@mahonkin.com>
2023-01-04 22:08:32 -0500

    added comuunity toolkit

--- TeaTimer/MauiProgram.cs
+++ TeaTimer/MauiProgram.cs
@@ -1,20 +1,20 @@
 using Microsoft.Maui.Controls.Hosting;
 using Microsoft.Maui.Hosting;
+using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Markup;
 
 namespace com.mahonkin.tim.maui.TeaTimer;
-/// <summary>
-/// MauiProgram application entry point
-/// </summary>
+
+/// <inheritdoc cref="Microsoft.Maui.Hosting.MauiApp" />
 public static class MauiProgram
 {
-    /// <summary>
-    /// Create the App
-    /// </summary>
-    /// <returns>The generated MauiApp</returns>
+    /// <inheritdoc cref="Microsoft.Maui.Hosting.MauiApp.CreateBuilder(bool)" />
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder()
         .UseMauiApp<App>()
+        .UseMauiCommunityToolkit()
+        .UseMauiCommunityToolkitMarkup()
         .ConfigureFonts(fonts =>
         {
             fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
