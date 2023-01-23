935f6ad5782cff1abfdb909bafdb2eaa0b3ad4ab
Timothy Reynolds <tim@mahonkin.com>
2023-01-23 16:39:31 -0500

    Added ThemeCange event handler  Changes to be committed: 	modified:   TeaTimer/App.xaml.cs 	modified:   TeaTimer/App.xaml

--- TeaTimer/App.xaml.cs
+++ TeaTimer/App.xaml.cs
@@ -6,27 +6,16 @@ using Microsoft.Maui.Storage;
 
 namespace com.mahonkin.tim.maui.TeaTimer;
 
-/// <summary>
 /// <inheritdoc cref="Application" />
-/// </summary>
 public partial class App : Application
 {
-    #region Public Properties
-    public static readonly string SharedPrefsName = Assembly.GetExecutingAssembly().GetName().Name;
-    public static bool UseCelsius;
-    public static AppTheme CurrentAppTheme;
-    #endregion
-
-    /// <summary>
     /// <inheritdoc cref="Application.Application()" />
-    /// </summary>
     public App(INavigationService navigationService)
     {
         InitializeComponent();
         MainPage = new AppShell(navigationService);
-        UseCelsius = Preferences.Get(nameof(UseCelsius), false, SharedPrefsName);
-        CurrentAppTheme = (AppTheme)Preferences.Get(nameof(CurrentAppTheme), (int)RequestedTheme, SharedPrefsName);
-        UserAppTheme = CurrentAppTheme;
+        UserAppTheme = RequestedTheme;
+        RequestedThemeChanged += (sender, args) => UserAppTheme = args.RequestedTheme;
     }
 }
 
