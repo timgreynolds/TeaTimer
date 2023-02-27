ee305d9d84fe0e9c858f08c5370312089a03ef4c
Tim Reynolds <tim@mahonkin.com>
2023-02-27 12:22:27 -0500

    Reference API Controller application
     Changes to be committed:
    	new file:   TeaApi/Controllers/TeasController.cs
    	new file:   TeaApi/Program.cs
    	new file:   TeaApi/Properties/launchSettings.json
    	new file:   TeaApi/TeaApi.csproj
    	new file:   TeaApi/appsettings.json

---
+++ TeaApi/Controllers/TeasController.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeaApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TeasController : ControllerBase
+{
+    private readonly ILogger<TeasController> _logger;
+
+    public TeasController(ILogger<TeasController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet()]
+    public IActionResult Teas()
+    {
+        _logger.LogTrace("Get Teas Action");
+        return new OkObjectResult(new[] { new { Name = "Earl Grey", SteepTime = TimeSpan.FromMinutes(2), BrewTemp = 212 },
+                                          new { Name = "Lady Grey", SteepTime = TimeSpan.FromSeconds(90), BrewTemp = 185 }});
+    }
+
+    [HttpGet("{Id}"), ActionName("GetById")]
+    public IActionResult Teas(string Id)
+    {
+        _logger.LogTrace("Get Tea by Id Action");
+        return new OkObjectResult(new[] { new { Id = 1, Name = "Earl Grey", SteepTime = TimeSpan.FromMinutes(2), BrewTemp = 212 } });
+    }
+
+    [HttpPost()]
+    public IActionResult Teas([FromHeader(Name = "Host")] string host, [FromBody] JsonDocument body)
+    {
+        int id = 0;
+        if (body.RootElement.TryGetProperty("Id", out JsonElement idElement))
+        {
+            id = idElement.GetInt32();
+        }
+        _logger.LogTrace($"Post Tea Action {body}");
+        return new CreatedResult($"http://{host}/api/teas/{id}", body);
+    }
+}
