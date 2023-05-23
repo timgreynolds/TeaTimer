20b6ec2d56fe6fc8e8424a347ee08963ec594f08
Tim Reynolds <tim@mahonkin.com>
2023-05-23 18:11:34 -0400

    Added logic to save state when sent to the background on iOS and restore state when brought back into the foreground.
     Changes to be committed:
    	modified:   TeaTimer/Platforms/MacCatalyst/AppDelegate.cs
    	modified:   TeaTimer/Platforms/iOS/AppDelegate.cs
    	modified:   TeaTimer/Platforms/iOS/Program.cs

--- TeaTimer/Platforms/iOS/AppDelegate.cs
+++ TeaTimer/Platforms/iOS/AppDelegate.cs
@@ -1,4 +1,7 @@
-﻿using com.mahonkin.tim.maui.TeaTimer.Platforms.iOS;
+﻿using System;
+using System.Reflection;
+using com.mahonkin.tim.maui.TeaTimer.Platforms.iOS;
+using com.mahonkin.tim.maui.TeaTimer.ViewModels;
 using Foundation;
 using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
@@ -10,7 +13,60 @@ namespace com.mahonkin.tim.maui.TeaTimer;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
+    private TimeSpan _timeLeft = TimeSpan.Zero;
+    private DateTime _backgroundedTime = DateTime.UtcNow;
+    private object _currentBindingContext;
+    private CoreFoundation.OSLog _logger = new CoreFoundation.OSLog(Assembly.GetExecutingAssembly().GetName().Name, nameof(AppDelegate));
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+    public override void DidEnterBackground(UIApplication application)
+    {
+        _logger.Log(CoreFoundation.OSLogLevel.Info, $"====Entering background====");
+        Console.WriteLine($"====Countdown left: {_timeLeft.ToString(@"mm\:ss")}");
+        Console.WriteLine($"====Backgrounded at: {_backgroundedTime.ToLocalTime().ToString(@"hh\:mm\:ss")}");
+        _currentBindingContext = AppShell.Current.CurrentPage.BindingContext;
+        Console.WriteLine($"====Page viewmodel type: {_currentBindingContext.GetType().Name}");
+        if (_currentBindingContext.GetType().IsAssignableTo(typeof(TimerViewModel)))
+        {
+            Console.WriteLine($"====Saving state====");
+            _timeLeft = ((TimerViewModel)_currentBindingContext).CountdownLabel;
+            _backgroundedTime = DateTime.UtcNow;
+            Console.WriteLine($"====Countdown left: {_timeLeft.ToString(@"mm\:ss")}");
+            Console.WriteLine($"====Backgrounded at: {_backgroundedTime.ToLocalTime().ToString(@"hh\:mm\:ss")}");
+        }
+        base.DidEnterBackground(application);
+        Console.WriteLine($"====Entered background=====");
+    }
+
+    public override void WillEnterForeground(UIApplication application)
+    {
+        Console.WriteLine($"====Entering foreground====");
+        Console.WriteLine($"====Countdown left when backgrounded: {_timeLeft.ToString(@"mm\:ss")}");
+        Console.WriteLine($"====Backgrounded at: {_backgroundedTime.ToLocalTime().ToString(@"hh\:mm\:ss")}");
+        Console.WriteLine($"====Awoken at: {DateTime.UtcNow.ToLocalTime().ToString(@"hh\:mm\:ss")}");
+        TimeSpan elapsedTime = DateTime.UtcNow.Subtract(_backgroundedTime);
+        Console.WriteLine($"====Time spent in background: {elapsedTime.ToString(@"mm\:ss")}");
+        TimeSpan remainingTime = _timeLeft.Subtract(elapsedTime);
+        Console.WriteLine($"====Page viewmodel type: {_currentBindingContext.GetType().Name}");
+        if (_currentBindingContext.GetType().IsAssignableTo(typeof(TimerViewModel)))
+        {
+            Console.WriteLine($"====Resetting state====");
+            if (remainingTime > TimeSpan.Zero)
+            {
+                Console.WriteLine($"====Current time remaining: {remainingTime.ToString(@"mm\:ss")}");
+                ((TimerViewModel)_currentBindingContext).CountdownLabel = remainingTime;
+                Console.WriteLine($"====Page Countdown set to: {remainingTime.ToString(@"mm\:ss")}");
+            }
+            else
+            {
+                Console.WriteLine($"====Timer already expired====");
+                ((TimerViewModel)_currentBindingContext).CountdownLabel = TimeSpan.Zero;
+            }
+            Console.WriteLine($"====State reset====");
+        }
+        base.WillEnterForeground(application);
+        Console.WriteLine($"====Entered foreground====");
+    }
 
     public override bool WillFinishLaunching(UIApplication application, NSDictionary launchOptions)
     {
--- TeaTimer/Platforms/iOS/Program.cs
+++ TeaTimer/Platforms/iOS/Program.cs
@@ -7,9 +7,18 @@ public class Program
 	// This is the main entry point of the application.
 	static void Main(string[] args)
 	{
-        // if you want to use a different Application Delegate class from "AppDelegate"
-        // you can specify it here.
-        UIApplication.Main(args, null, typeof(AppDelegate));
+		// if you want to use a different Application Delegate class from "AppDelegate"
+		// you can specify it here.
+		try
+		{
+			UIApplication.Main(args, null, typeof(AppDelegate));
+		}
+		catch(System.Exception ex)
+		{
+			System.Console.WriteLine($"Application level exception handler. Something went wrong.");
+			System.Console.WriteLine(ex.Message);
+			System.Console.WriteLine(ex.StackTrace);
+		}
 	}
 }
 
