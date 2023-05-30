5154bfb04d3fb37d2636ba9d1ab0a030ef5c3925
Tim Reynolds <tim@mahonkin.com>
2023-05-30 19:07:09 -0400

    Added IsTimerRunning property to the TimerViewModel.
     Changes to be committed:
    	modified:   TeaTimer/Platforms/iOS/TimerViewModel.cs

--- TeaTimer/Platforms/iOS/TimerViewModel.cs
+++ TeaTimer/Platforms/iOS/TimerViewModel.cs
@@ -5,6 +5,8 @@ namespace com.mahonkin.tim.maui.TeaTimer.ViewModels
 {
     public partial class TimerViewModel
     {
+        public bool IsTimerRunning => _timerService.IsRunning;
+
         async private partial Task TimerExpired()
         {
             SystemSound alarmSound = new SystemSound(1005);
