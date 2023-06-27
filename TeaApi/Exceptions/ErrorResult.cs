96c23d5ed3c8dade678451f0d863501ae42ff139
Tim Reynolds <tim@mahonkin.com>
2023-06-27 13:23:55 -0400

    Modified the TeaApi web service to actually use the database.
     Changes to be committed:
    	modified:   TeaApi/Controllers/TeasController.cs
    	new file:   TeaApi/Exceptions/ErrorResult.cs
    	new file:   TeaApi/Exceptions/InternalServerErrorObjectResult.cs
    	new file:   TeaApi/Models/TeaModel.cs
    	modified:   TeaApi/Program.cs
    	new file:   TeaApi/Services/IDataService.cs
    	new file:   TeaApi/Services/TeaSqlService.cs
    	modified:   TeaApi/TeaApi.csproj
    	new file:   TeaApi/TeaApi.sln

---
+++ TeaApi/Exceptions/ErrorResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace com.mahonkin.tim.TeaApi.Exceptions
+{
+    [JsonSerializable(typeof(ErrorResult))]
+    internal class ErrorResult
+    {
+        [JsonInclude]
+        public System.Net.HttpStatusCode ErrorCode;
+        [JsonInclude]
+        public string Message;
+
+        [JsonConstructor]
+        public ErrorResult(System.Net.HttpStatusCode code, string message)
+        {
+            ErrorCode = code;
+            Message = message;
+        }
+    }
+}
---
+++ TeaApi/Exceptions/InternalServerErrorObjectResult.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace com.mahonkin.tim.TeaApi.Exceptions
+{
+    public class InternalServerErrorObjectResult: ObjectResult
+	{
+        public InternalServerErrorObjectResult(object value) : base(value)
+		{
+            StatusCode = 500;
+		}
+	}
+}
+
