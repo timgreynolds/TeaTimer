3c8530bbc5590957eb7118d354364c1c18d2dca2
Tim Reynolds <tim@mahonkin.com>
2023-09-07 18:55:35 -0400

    Cleaned up formatting.
     Changes to be committed:
    	modified:   TeaTimer/Platforms/iOS/Program.cs

--- TeaTimer/Platforms/iOS/Program.cs
+++ TeaTimer/Platforms/iOS/Program.cs
@@ -7,22 +7,22 @@ namespace com.mahonkin.tim.maui.TeaTimer;
 /// </summary>
 public class Program
 {
-	private static CoreFoundation.OSLog _logger = new CoreFoundation.OSLog(System.Reflection.Assembly.GetExecutingAssembly().GetName().Name, "Crash");
+    private static CoreFoundation.OSLog _logger = new CoreFoundation.OSLog(System.Reflection.Assembly.GetExecutingAssembly().GetName().Name, "Crash");
 
     /// <inheritdoc cref="UIApplication.Main(string[], System.Type, System.Type)" />
     static void Main()
-	{
-		// if you want to use a different Application Delegate class from "AppDelegate"
-		// you can specify it here.
-		try
-		{
-			UIApplication.Main(null, null, typeof(AppDelegate));
-		}
-		catch(System.Exception ex)
-		{
+    {
+        // if you want to use a different Application Delegate class from "AppDelegate"
+        // you can specify it here.
+        try
+        {
+            UIApplication.Main(null, null, typeof(AppDelegate));
+        }
+        catch (System.Exception ex)
+        {
             _logger.Log(CoreFoundation.OSLogLevel.Fault, $"Application level exception handler. Something went wrong.\n" +
                 $"{ex.Message}\n{ex.StackTrace}");
-		}
-	}
+        }
+    }
 }
 
