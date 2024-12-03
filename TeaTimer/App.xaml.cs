a5de88ab6609cd3c9623504bace34ec16c9d3ed3
Tim Reynolds <timgreynolds@icloud.com>
2024-12-02 21:11:21 -0500

    Updated to remove the deprecated MainPage property.
     Changes to be committed:
    	modified:   TeaTimer/App.xaml.cs

--- TeaTimer/App.xaml.cs
+++ TeaTimer/App.xaml.cs
@@ -2,6 +2,7 @@ using com.mahonkin.tim.maui.TeaTimer.Services;
 using com.mahonkin.tim.TeaDataService.DataModel;
 using com.mahonkin.tim.TeaDataService.Services;
 using Microsoft.Extensions.Logging;
+using Microsoft.Maui;
 using Microsoft.Maui.Controls;
 
 namespace com.mahonkin.tim.maui.TeaTimer
@@ -9,16 +10,30 @@ namespace com.mahonkin.tim.maui.TeaTimer
     /// <inheritdoc cref="Application" />
     public partial class TeaTimerApp : Application
     {
+        private INavigationService _navigationService;
+        private IDataService<TeaModel> _sqlService;
+        private ISettingsService _settingsService;
+        private ILoggerFactory _loggerFactory;
+
         /// <inheritdoc cref="Application.Application()" />
         public TeaTimerApp(INavigationService navigationService, IDataService<TeaModel> sqlService, ISettingsService settingsService, ILoggerFactory loggerFactory)
         {
-           loggerFactory.CreateLogger<TeaTimerApp>().LogTrace("Constructor entered.");
+            _navigationService = navigationService;
+            _sqlService = sqlService;
+            _settingsService = settingsService;
+            _loggerFactory = loggerFactory;
+
+            _loggerFactory.CreateLogger<TeaTimerApp>().LogTrace("Constructor entered.");
 
             InitializeComponent();
-            
-            MainPage = new AppShell(navigationService, sqlService, settingsService, loggerFactory);
+
             UserAppTheme = Microsoft.Maui.ApplicationModel.AppTheme.Light;
             RequestedThemeChanged += (sender, args) => UserAppTheme = args.RequestedTheme;
+        }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            return new Window(new AppShell(_navigationService, _sqlService, _settingsService, _loggerFactory));
         }
     }
 }
